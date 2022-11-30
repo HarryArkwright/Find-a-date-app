35ba29049ab7bed506a54a72e46cefe24a9f66a3
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-11-30 14:08:52 +0000

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -38,7 +38,7 @@ const generateAction = async (req, res) => {
     // I set a higher temperature for this one. Up to you!
     temperature: 0.85,
 		// I also increase max_tokens.
-    max_tokens: 1250,
+    max_tokens: 750,
   });
   
   // Get the output
