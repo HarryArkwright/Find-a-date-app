ada4e9cbfbc352170495db5b796431d3986b1f50
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-11-30 15:13:16 +0000

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,6 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = "";
+
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
@@ -20,34 +21,7 @@ const generateAction = async (req, res) => {
   
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  const secondPrompt = 
-  `
-  Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
-
-  Title: ${req.body.userInput}
-
-  Table of Contents: ${basePromptOutput.text}
-
-  Blog Post:
-  `
-  
-  // I call the OpenAI API a second time with Prompt #2
-  const secondPromptCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${secondPrompt}`,
-    // I set a higher temperature for this one. Up to you!
-    temperature: 0.85,
-		// I also increase max_tokens.
-    max_tokens: 750,
-  });
-  
-  // Get the output
-  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
-
-
-
-
-  res.status(200).json({ output: secondPromptOutput });
+  res.status(200).json({ output: basePromptOutput });
 };
 
 export default generateAction;
