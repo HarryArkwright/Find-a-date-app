4ae737d0cfb40ad350ccc2cd43fdbfd27fbc754d
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-12-01 13:23:37 +0000

    latest build

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ const callGenerateEndpoint = async () => {
             <h1>Magic Chat Up Line Generator. Find a Date</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Use AI to find a date. Tell us what you want your chat up line to be about ( ex. Write me a funny chat up line about, Give me a chat up line so I won't be alone forever ).  </h2>
+            <h2>Use AI to find a date. Tell us what you want your chat up line to be about ( ex. Write me a funny chat up line about flowers, Give me a chat up line so I won't be alone forever ).  </h2>
           </div>
         </div>
                 <div className="prompt-container">
